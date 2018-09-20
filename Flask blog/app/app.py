9a3c5fd1d1d5a5f45678c52e5cea95600632c432
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-09-20 18:22:39 +0300

    указали пути на views

--- Flask blog/app/app.py
+++ Flask blog/app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from config import Configuration
 
+from posts.blueprint import posts
+
 
 app = Flask(__name__) #__name__ это имя теукщего файла app.py (необходимо указывать это отталкивающая точка)
 app.config.from_object(Configuration)
+
+app.register_blueprint(posts, url_prefix='/blog') # регистрируем наш blueprint
