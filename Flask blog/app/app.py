b0778f03662be11731064467ac4b25d07434a162
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-09-21 20:39:38 +0300

    подключили mysql

--- Flask blog/app/app.py
+++ Flask blog/app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Configuration
+from flask_sqlalchemy import SQLAlchemy
 
 from posts.blueprint import posts
 
 
 app = Flask(__name__) #__name__ это имя теукщего файла app.py (необходимо указывать это отталкивающая точка)
 app.config.from_object(Configuration)
+
+db = SQLAlchemy(app)
 
 app.register_blueprint(posts, url_prefix='/blog') # регистрируем наш blueprint
