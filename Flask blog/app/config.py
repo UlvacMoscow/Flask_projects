59753b47f88941b619af1aa8821375a864e5e63c
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-09-29 21:59:15 +0300

    добавил в админку колонку с тегами

--- Flask blog/app/config.py
+++ Flask blog/app/config.py
@@ -2,3 +2,4 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://mysql:mysql@localhost/testdb'
+    SECRET_KEY = 'secret key'
