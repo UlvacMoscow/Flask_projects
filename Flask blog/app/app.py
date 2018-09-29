aa69ce0f69fb627e58eb08c4bd6dd62a828d69f4
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-09-29 21:51:57 +0300

    добавил админку и колонку с постами

--- Flask blog/app/app.py
+++ Flask blog/app/app.py
@@ -5,6 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
 app = Flask(__name__) #__name__ это имя теукщего файла app.py (необходимо указывать это отталкивающая точка)
 app.config.from_object(Configuration)
 
@@ -13,3 +17,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+### Admin ###
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session()))
