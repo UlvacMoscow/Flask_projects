987b202b53238f8194dcecbb0efdbbc8e6a05c7d
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-10-01 18:05:27 +0300

    Добавил класс User и Role, и таблицу в базу данных для хранения User

--- Flask blog/app/models.py
+++ Flask blog/app/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import re
 from sqlalchemy.sql.functions import current_time
 
+from flask_security import UserMixin, RoleMixin
+
 
 def slugify(str):  #pythex.org проверяем регулярку
     pattern = r'[^\w+]'
@@ -49,3 +51,25 @@ class Tag(db.Model):
 
     def __repr__(self):
         return '{}'.format(self.name)
+
+
+### Flask security
+
+Roles_users = db.Table('roles_users',
+            db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+            db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+            )
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    emeil = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
